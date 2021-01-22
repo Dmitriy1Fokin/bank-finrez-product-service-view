440d9f25fcebb1e084b715ecdbae91b541973158
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-22 17:00:49 +0300

    [BF-76] first commit. Init empty project.

---
+++ src/main/kotlin/ru/fdo/bank/finrez/productserviceview/ProductServiceViewApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bank.finrez.productserviceview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductServiceViewApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductServiceViewApplication>(*args)
+}
---
+++ src/test/kotlin/ru/fdo/bank/finrez/productserviceview/ProductServiceViewApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.fdo.bank.finrez.productserviceview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductServiceViewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
